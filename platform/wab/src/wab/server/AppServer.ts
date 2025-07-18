4b6267a2d9108ddb5f35aea601b55f87a97b57d8
hyunnbunt <rha542@gmail.com>
2025-07-18 11:32:24 +0900

    connect request to deactivateSelfUserof routes.ts

--- platform/wab/src/wab/server/AppServer.ts
+++ platform/wab/src/wab/server/AppServer.ts
@@ -1299,6 +1299,7 @@ export function addMainAppServerRoutes(
     sensitiveRateLimiter,
     withNext(authRoutes.updateSelfPassword)
   );
+  app.post("/api/v1/auth/self/deactivate-user", withNext(authRoutes.deactivateSelfUser)); 
   app.post("/api/v1/auth/logout", withNext(authRoutes.logout));
   app.post(
     "/api/v1/auth/forgotPassword",
